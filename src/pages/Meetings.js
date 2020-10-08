a44d3e9458fde5fe9b0e82e23c2f07d2496a484a
rstrasser04 <69007424+rstrasser04@users.noreply.github.com>
2020-10-07 21:19:33 -0500

    Update Meetings.js

--- src/pages/Meetings.js
+++ src/pages/Meetings.js
@@ -25,13 +25,10 @@ const Meetings = () => (
                 <Notes />
               </div>
             </section>
-<<<<<<< HEAD
-=======
             <section className="right meetings">
             <h1>Chat</h1>
                 <p>The Chat feature is currently unavailable. We are working hard to get this fixed and being able to provide you with this feature. In the meantime, please feel free to utilize the Notes section below to take any of your notes. If there are any questions from the presentations, please email us at <a href="mailto:educationseminars@fiserv.com" target="_blank">educationseminars@fiserv.com</a>.</p> 
             </section>
->>>>>>> f8fa350412443c5a790c0527944769cfec586ee9
           </div>
           
         </div>
