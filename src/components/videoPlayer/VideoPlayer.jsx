c4cee90b9e160bd9cc22081c341e5dd522291f09
rstrasser04 <69007424+rstrasser04@users.noreply.github.com>
2020-10-23 14:46:30 -0500

    Update VideoPlayer.jsx

--- src/components/videoPlayer/VideoPlayer.jsx
+++ src/components/videoPlayer/VideoPlayer.jsx
@@ -90,36 +90,6 @@ class VideoPlayer extends Component {
         <div className="aspect-169 pos-relative full-width full-height">
           <video id="video-player" className="video-elem pos-absolute full-width" playsInline controls></video>
         </div>
-        <div style={{color: "red", textAlign: "center", fontWeight: "normal"}}>
-          <h3 style={{fontWeight: "normal"}}>
-            **If the video does not start with sound, please hover over the video and make sure the player is not on mute.**
-          </h3>
-          <h3 style={{fontWeight: "normal"}}>
-            **If you are having any technical difficulties with the video, please visit the FAQ page on the left sidebar to help answer your questions.**
-          </h3>
-        </div>
-        <div>
-          <p>
-            <a href="https://urldefense.proofpoint.com/v2/url?u=https-3A__fiserventerprise.webex.com_fiserventerprise_k2_j.php-3FMTID-3Dt7fa8dea652004f624e1a2e50b81b1cf1&d=DwMGaQ&c=rE3mhBYFJfJGqQ7WI0-DPw&r=lD_m483ZNdrPoub1glQ0NI-p62b7YfEyJ2JEVPaf7E0&m=eL_OJA6ItHfgdTXuNYO04nDzrciyBkaShThVIkeVjTo&s=vXbER_VXbF9CEk6kTiaGZM4gaaO28hjFbujwcdZOx2U&e=" target="_blank">BPM Networking</a><br/>
-            Session number: <b>131 082 7617</b><br/> 
-            Session password: <b>cmUpXFd?855</b>
-          </p>
-          <p>
-            <a href="https://urldefense.proofpoint.com/v2/url?u=https-3A__fiserventerprise.webex.com_fiserventerprise_k2_j.php-3FMTID-3Dt3efaaa7f350754f687c4919bf5e49984&d=DwMGaQ&c=rE3mhBYFJfJGqQ7WI0-DPw&r=lD_m483ZNdrPoub1glQ0NI-p62b7YfEyJ2JEVPaf7E0&m=M-DqMWVdSHP4W2T3BHd-Xlv4IQ16tBfVb2wOepeHMD8&s=TZYhwlMIItf9crCiHfo2fA_w24hiNZreCPVlbfMyUAM&e=" target="_blank">Deposit Networking</a><br/>
-            Session number: <b>131 078 2233</b><br/>
-            Session password: <b>5v2KwEgJC@9</b> 
-          </p>
-          <p>
-            <a href="https://urldefense.proofpoint.com/v2/url?u=https-3A__fiserventerprise.webex.com_fiserventerprise_k2_j.php-3FMTID-3Dt0d19b04ae9021e6470d89555aab8ed84&d=DwMGaQ&c=rE3mhBYFJfJGqQ7WI0-DPw&r=lD_m483ZNdrPoub1glQ0NI-p62b7YfEyJ2JEVPaf7E0&m=h2b0TsUdPeTNVxH5Zs1rG9r_ArM47mS1ApFy9z8ODqU&s=RXre_uzBdOZszQarL7ZZ5hFsJGh-DHwBiqt9GzbXzn8&e=" target="_blank">Loan Networking</a><br/>
-            Session number: <b>131 570 2566</b><br/>
-            Session password: <b>mVghir6B*82</b>
-          </p>
-          <p>
-            <a href="https://emeraldconnect.webex.com/emeraldconnect/k2/j.php?MTID=tffd1fa4b56fa54081e3308546833012e" target="_blank">Business Online Networking</a><br/>
-            Session number: <b>126 630 6771</b><br/>
-            Session password: <b>QmPbQxRd635</b>
-          </p>
-        </div>
       </div>
     )
   }
