828e4b6b5a556363c423b42847ee091582bd804b
Strasser <ryan.strasser@fiserv.com>
2021-07-07 15:12:46 -0500

    updated blurb and header

--- src/containers/Home/Left.js
+++ src/containers/Home/Left.js
@@ -9,15 +9,18 @@ class Left extends Component {
             <div className="greetings">
               <img
               class='mb-3 app-logo'
-              src='https://seminar-media.s3.amazonaws.com/Spring/2021/home-banner.png'
-              alt='2021 Precision Spring Conference'
+              src='https://seminar-media.s3.amazonaws.com/Fall/2021/fall-header.png'
+              alt='2021 Precision Fall Conference'
               width=''
             />
             </div>
             
             <h1>Introduction</h1>
             <p>
-              Now is the time to join Fiserv experts and industry peers for this year’s Precision Knowledge Exchange. Optimize your organization’s performance and drive home valuable software and procedural insights during this comprehensive three-day seminar. Fiserv experts facilitate interactive presentations and discussions designed to inform, educate, and empower. Leverage this virtual venue to learn more about the software you use every day and share your best practices and software tips all while building a diverse network of relationships.
+              Now is the ideal time to focus on professional development and optimization of your organization’s performance. Collaborate with Fiserv experts and industry peers for this year’s Fall Education Seminars. This year’s event will provide participants with valuable software and procedural insights throughout the comprehensive three-day seminar. Fiserv experts facilitate interactive presentations and discussions designed to inform, educate, and empower. Attendees will leverage a dynamic, virtual venue while learning about Precision software and best practices all while building a diverse network of professional relationships.
+            </p>
+            <p>
+              The 2021 Precision Fall Education Seminar is the affordable and convenient training solution your organization deserves. Concerned about documenting all of the amazing information the seminars have to offer? This year’s participant’s will gain access to unlimited recordings for three months after the event concludes. Fiserv is also offering unlimited connections for one, low price. Register Now and begin your institution’s journey towards efficiency!
             </p>
             <h2>
               Seminar Dates:
